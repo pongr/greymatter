3399badd6fa44d10f4d0640a9f2dd3193109b10f
zcox <zcox522@gmail.com>
2011-07-10 20:30:15 -0500

    Initial code commit

---
+++ project/build/Project.scala
@@ -0,0 +1,24 @@
+import sbt._
+import assembly.AssemblyBuilder
+
+class Project(info: ProjectInfo) 
+    extends DefaultProject(info) 
+    with AkkaProject 
+    with AssemblyBuilder
+    with LicenseHeaders
+    with ApacheLicense2 {
+  
+  def copyrightLine = "Copyright (c) 2011 Pongr, Inc."
+  
+  val javaNetRepo = "javaNetRepo" at "http://download.java.net/maven/2/"
+  
+  val mailetBase = "org.apache.james" % "apache-mailet-base" % "1.1"
+  
+  //James logs using log4j, so include slf4j-log4j12 but exclude log4j since James already has it
+  override def ivyXML =
+    <dependencies>
+      <dependency org="org.slf4j" name="slf4j-log4j12" rev="1.6.0">
+        <exclude org="log4j" name="log4j" />
+      </dependency>
+    </dependencies>
+}
---
+++ src/main/scala/LogMailet.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2011 Pongr, Inc.
+ *   
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pongr.greymatter.example
+
+import com.pongr.greymatter.GreyMatterMailet
+import org.apache.mailet.{ Mail, MailAddress }
+import akka.actor.Actor
+import akka.actor.Actor.actorOf
+import scala.collection.JavaConversions._
+
+/** Uses [[com.pongr.akkamailet.LogActor]] to receive all mail messages. */
+class LogMailet extends GreyMatterMailet {
+  def newActor = actorOf[LogActor].start
+
+  def messageFor(mail: Mail) = LogMessage(
+    mail.getName,
+    mail.getSender,
+    mail.getRecipients map (_.asInstanceOf[MailAddress]))
+
+  override def ghost(mail: Mail) = true
+
+  override def init() { log("LogMailet starting up...") }
+}
+
+/** Simple message to send to [[com.pongr.akkamailet.LogActor]]. */
+case class LogMessage(name: String, sender: MailAddress, recipients: Iterable[MailAddress])
+
+/** Logs each received mail message. */
+class LogActor extends Actor {
+  def receive = {
+    case LogMessage(name, sender, recipients) => 
+      log.info("Received mail " + name + " from " + sender + " to " + recipients)
+    case msg@_ => log.warn("Unknown message: " + msg)
+  }
+}
